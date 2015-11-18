ff8b5692108b1af5e2932eb91311f50d040b11c2
James M. Lawrence <llmjjmll@gmail.com>
2015-11-18 07:25:36 -0500

    don't pollute cl-user with loop keywords (#20)

--- src/package.lisp
+++ src/package.lisp
@@ -28,6 +28,8 @@
 ;;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 ;;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+(in-package #:lparallel.kernel)
+
 (macrolet
     ((package (package-name documentation &rest list)
        `(defpackage ,package-name
